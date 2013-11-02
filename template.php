844ace2f6b01321712b15542d47746c6a40b699f
alexz-enwp <mrzmanwiki@gmail.com>
2013-11-01 23:55:17 -0400

    updates

--- template.php
+++ template.php
@@ -1,10 +1,28 @@
 <?php
+/*
+	Copyright 2013 Alex Zaddach. (mrzmanwiki@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 function templatetop( $title, $css = array(), $js = array(), $subpage = '', $headother = '' ) {
 	global $PROJECT;
 	$csslinks = "";
 	foreach( $css as $page ) {
 		$csslinks .= "		<link rel=\"stylesheet\" href=\"/$PROJECT/commonphp/$page\" type=\"text/css\" />\n";
 	}
+	// NOTE: Not currently implemented!
 	$title = htmlspecialchars( $title, ENT_QUOTES );
 	if ( $subpage ) {
 		$subpage = "<span class=\"subpages\">&lt; $subpage</span>";
@@ -19,7 +37,7 @@ function templatetop( $title, $css = array(), $js = array(), $subpage = '', $hea
 <html>
 	<head>
 		<title>$title - alexz's tools - Wikimedia Tool Labs</title>
-		<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
+		<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 $csslinks
 		<link rel=\"stylesheet\" href=\"/$PROJECT/commonphp/main.css\" type=\"text/css\" media=\"screen\" />
 
@@ -27,87 +45,39 @@ $jselems
 $headother
 	</head>
 
-<body class=\"mediawiki ltr skin-modern\">
+<body>
+<div class='bgcontainer'><div class='container'>
+<div class='header'><h1 class='header'>Tool Labs: alexz's tools</h1></div>
 
-	<!-- heading -->
-	<div id=\"mw_header\"><h1 id=\"firstHeading\">$title</h1></div>
+<div class='left'>
+<h4>Information</h4>
+<ul class='sidebarlinks'>
+<li class='slink'><a href='https://tools.wmflabs.org/' title='Tool Labs'>Wikimedia Tool Labs</a></li>
+<!--<li class='slink'><a href='' title=''>More of alexz's tools</a></li>
+<li class='slink'><a href='' title=''>Admins willing to make difficult blocks</a></li>-->
+<li class='slink'><a href='https://en.wikipedia.org/wiki/User:Mr.Z-bot' title='Mr.Z-bot'>Bot info</a></li>
+<li class='slink'><a href='https://github.com/alexz-enwp?tab=repositories' title='Source'>Source code</a></li>
+</ul>
+<h4>Contact</h4>
+<ul class='sidebarlinks'>
+<li class='slink'><a href='https://en.wikipedia.org/wiki/User_talk:Mr.Z-man' title='Talk page'>Talk page</a></li>
+</ul>
+</div>
 
-	<div id=\"mw_main\">
-	<div id=\"mw_contentwrapper\">
-
-	<!-- content -->
-
-	<div id=\"mw_content\">
-	<!-- contentholder does nothing by default, but it allows users to style the text inside
-	     the content area without affecting the meaning of 'em' in #mw_content, which is used
-	     for the margins -->
-	<div id=\"mw_contentholder\">
-
-		<div id=\"contentSub\">$subpage</div>
+<div class='content'>
+<h2>$title</h2>
 ";
 }
 
 function templatebottom() {
 
 	echo "
-		<div class='mw_clear'></div>
-					</div><!-- mw_contentholder -->
-	</div><!-- mw_content -->
-	</div><!-- mw_contentwrapper -->
-
-	<div id=\"mw_portlets\">
-
-	<!-- portlets -->
-	<div class='generated-sidebar portlet' id='p-navigation'>
-		<h5>Navigation</h5>
-
-		<div class='pBody'>
-			<ul>
-				<li><a href=\"https://wiki.toolserver.org/\" title=\"Toolserver wiki\" >Toolserver wiki</a></li>
-				<li><a href=\"http://toolserver.org/~alexz/\" title=\"More tools\" >More tools</a></li>
-				<li><a href=\"http://toolserver.org/~alexz/coord\" title=\"Geotagged pages needing images\" >Geotagged pages needing images</a></li>
-				<li><a href=\"http://toolserver.org/~alexz/pop/\" title=\"Popular pages\">WikiProject popular pages</a></li>
-				<li><a href=\"http://toolserver.org/~alexz/amdb/\" title=\"AMDB\">Admins willing to make difficult blocks</a></li>
-				<li><a href=\"http://en.wikipedia.org/wiki/User:Mr.Z-bot\" title=\"Bots\">Bots</a></li>
-				<li>Contact: <a href=\"http://en.wikipedia.org/wiki/User_talk:Mr.Z-man\" title=\"Talk page\">Talk page</a></li>
-				<li><a href=\"https://jira.toolserver.org/secure/CreateIssue.jspa?pid=10310&issuetype=1\" title=\"Bug reports\">Bug reports</a></li>
-				<li><a href=\"https://jira.toolserver.org/secure/CreateIssue.jspa?pid=10310&issuetype=2\" title=\"Feature requests\">Feature requests</a></li>
-			</ul>
-		</div><!-- pBody -->
-		<h5>&nbsp;</h5>
-<!--		<p>
-			<a href=\"http://wiki.toolserver.org/\" title=\"Wikimedia Toolserver\">
-				<img src=\"/images/wikimedia-toolserver-button.png\" alt=\"Wikimedia Toolserver\"/>
-			</a>
-		</p>
-		<p>
-			<a title=\"Valid HTML 5\" href=\"http://validator.w3.org/check?uri=referer\">
-				<img src=\"http://toolserver.org/~alexz/valid-html5.png\" alt=\"Valid HTML 5\" height=\"31\" width=\"88\" />
-			</a>
-		</p>
--->
-	</div><!-- portlet -->
-
-	</div><!-- mw_portlets -->
+</div>
 
 
-	</div><!-- main -->
+<div class='footer'>Footer</div>
+</div></div>
 
-	<div class=\"mw_clear\"></div>
-
-	<!-- personal portlet -->
-
-	<div class=\"portlet\" id=\"p-personal\">
-		<h5>Personal tools</h5>
-		<div class=\"pBody\">
-			<ul>
-			</ul>
-		</div>
-	</div>
-
-
-	<!-- footer --> 
-	<div id=\"footer\">
-	</div>	
+	
 </body></html>";	
 }
