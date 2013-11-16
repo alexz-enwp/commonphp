36dff24875623c0ec540380fb3d52e563540c8de
alexz-enwp <mrzmanwiki@gmail.com>
2013-11-16 23:38:42 +0000

    rearrange, whitespace cleanup

--- license.php
+++ license.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 	Copyright 2013 Alex Zaddach. (mrzmanwiki@gmail.com)
 
@@ -16,37 +16,42 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
-echo"The PHP/HTML template, stylesheets, Javascript, supporting PHP scripts, and other parts of the interface except    
-those described below are licensed under the GNU General Public License version 3. 
+header('Content-Type:text/plain');
 
-The HTML5 logo: HTML5_1Color_Black.svg was produced by the W3C, is from http://www.w3.org/html/logo/ and is 
+// License details for the program
+echo file_get_contents('../license_info');
+
+echo '
+';
+
+// License for the template
+echo"The PHP/HTML template, stylesheets, Javascript, supporting PHP scripts, and other parts of the interface except
+those described below are licensed under the GNU General Public License version 3.
+
+The HTML5 logo: HTML5_1Color_Black.svg was produced by the W3C, is from http://www.w3.org/html/logo/ and is
 released under the Creative Commons Attribution 3.0 License - http://creativecommons.org/licenses/by/3.0/
 
-The GPLv3 logo: gpl-v3-logo.svg was produced by GNU, is from http://www.gnu.org/graphics/license-logos.html and 
-is released under the Creative Commons Attribution-NoDerivs 3.0 United States License - 
+The GPLv3 logo: gpl-v3-logo.svg was produced by GNU, is from http://www.gnu.org/graphics/license-logos.html and
+is released under the Creative Commons Attribution-NoDerivs 3.0 United States License -
 http://creativecommons.org/licenses/by-nd/3.0/us/
 
 The bullet image (https://upload.wikimedia.org/wikipedia/commons/9/90/Vector-bullet-icon.png) is public domain
 https://commons.wikimedia.org/wiki/File:Vector-bullet-icon.png
 
 The Tool Labs logo (http://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Tool_labs_logo_with_text.svg/150px-Tool_labs_logo_with_text.svg.png) 
-was produced by Brandon Harris, Wikimedia User:Isarra, and Wikimedia User:Danilo.mac 
-https://commons.wikimedia.org/wiki/File:Tool_labs_logo_with_text.svg and is released under the 
-Creative Commons Attribution-Share Alike 3.0 Unported license - 
+was produced by Brandon Harris, Wikimedia User:Isarra, and Wikimedia User:Danilo.mac
+https://commons.wikimedia.org/wiki/File:Tool_labs_logo_with_text.svg and is released under the
+Creative Commons Attribution-Share Alike 3.0 Unported license -
 https://creativecommons.org/licenses/by-sa/3.0/deed.en
- 
+
 Part of GlobalFunctions.php is from MediaWiki, which is released under GNU General Public License version 2.
 Authors: https://github.com/wikimedia/mediawiki-core/blob/master/CREDITS
 https://github.com/wikimedia/mediawiki-core/blob/master/includes/GlobalFunctions.php";
 
-echo '\n\n';
+echo '
 
-// License details for the program
-echo file_get_contents('../license_info');
 
-echo '\n\n';
+';
 
 // The GPL
 echo file_get_contents('LICENSE');
-
-echo '\n\n';
